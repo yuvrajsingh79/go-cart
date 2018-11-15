61f91e08e7d3f26ff3a2779aceefab29336a202d
yuvrajsingh79 <singhyuvraj79@gmail.com>
2018-11-15 09:21:31 +0530

    added auth events

--- webcontent/gocart/src/app/app.module.ts
+++ webcontent/gocart/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { PaymentComponent } from './payment/payment.component';
+import { MycartComponent } from './mycart/mycart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    DashboardComponent,
+    UsersComponent,
+    PaymentComponent,
+    MycartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,NgbModule,FormsModule,ReactiveFormsModule,HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
